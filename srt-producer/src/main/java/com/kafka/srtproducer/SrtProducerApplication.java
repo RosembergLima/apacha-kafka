ffa7213931fbb8dab00f317e175b852313a3ba8a
Rosemberg Bezerra <r.bezerra@kigroup.de>
2023-01-26 10:49:53 +0000

    srt-producer started

---
+++ srt-producer/src/main/java/com/kafka/srtproducer/SrtProducerApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.srtproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrtProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrtProducerApplication.class, args);
+	}
+
+}
---
+++ srt-producer/src/test/java/com/kafka/srtproducer/SrtProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.srtproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrtProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
