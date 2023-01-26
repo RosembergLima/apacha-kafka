cc128ee6adc4d29eba1152a5b639bf7ea5d989f5
Rosemberg Bezerra <r.bezerra@kigroup.de>
2023-01-26 15:57:02 +0000

    Added callback function and send message

---
+++ srt-producer/src/main/java/com/kafka/srtproducer/services/StringProducerService.java
@@ -0,0 +1,24 @@
+package com.kafka.srtproducer.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Log4j2
+@Service
+public class StringProducerService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message){
+        kafkaTemplate.send("srt-topic", message).whenComplete((result, exception) -> {
+            if (exception != null) {
+                log.error("Erro ao enviar mensagem para o kafka: ", exception);
+            } else {
+                log.info("Mensagem enviada com sucesso: {}", result);
+            }
+        });
+    }
+}
