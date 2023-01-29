ef14a4379a335986c18a493e08c932865601ff35
Rosemberg Bezerra <r.bezerra@kigroup.de>
2023-01-29 12:12:44 +0000

    added producer config

--- payment-service/src/main/java/com/kafka/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/kafka/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,14 +2,27 @@ package com.kafka.paymentservice.service.impl;
 
 import com.kafka.paymentservice.model.Payment;
 import com.kafka.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+
 @Log4j2
+@RequiredArgsConstructor
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
         log.info("Received ::: {}", payment);
+        Thread.sleep(1000);
+
+        log.info("Sending payment ::: {}", payment);
+        kafkaTemplate.send("payment-topic", payment);
     }
 }
